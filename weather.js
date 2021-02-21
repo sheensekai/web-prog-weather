a651e7f827033dba6f63c793c0ea6eafba5e724c
sheensekai <sheensekai@ya.ru>
2021-02-21 17:54:02 +0300

    Add req making, sending funcs

--- weather.js
+++ weather.js
@@ -1,4 +1,41 @@
+function getWeatherStateFromResponse(response) {
+    return {
+        "temp": response.main.temp,
+        "wind": response.wind.speed,
+        "clouds": response.clouds.all
+    };
+}
+
+function currentPositionSuccess(position) {
+    const source = {
+        byCity: false,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    }
+
+    const xhr = makeSourceWeatherRequest(source);
+    sendWeatherRequest(xhr, function() {
+        const state = getWeatherStateFromResponse(xhr.response);
+        console.log(state);
+    });
+}
+
+function currentPositionError() {
+    const def_latitude = 60;
+    const def_longitude = 30;
+    const source = {
+        byCity: false,
+        latitude: def_latitude,
+        longitude: def_longitude
+    }
+
+    const xhr = makeSourceWeatherRequest(source);
+    sendWeatherRequest(xhr, function() {
+        const state = getWeatherStateFromResponse(xhr.response);
+        console.log(state);
+    });
+}
+
 navigator.geolocation.getCurrentPosition(currentPositionSuccess, currentPositionError, {
     enableHighAccuracy: true
 })
-
--- weather_req.js
+++ weather_req.js
@@ -1,31 +1,41 @@
-function currentPositionSuccess(position) {
-    sendWeatherRequest(position.coords.latitude, position.coords.longitude);
-}
-
-function currentPositionError() {
-    const def_latitude = 60;
-    const def_longitude = 30;
-    sendWeatherRequest(def_latitude, def_longitude);
-}
-
-function sendWeatherRequest(latitude, longitude) {
+function makeWeatherRequest(params, func, async = true,) {
     const xhr = new XMLHttpRequest();
     const api_key = "34e35a1d80msh1acf606c8def3adp132cbbjsn7f1eb5c439c4";
     const host = "community-open-weather-map.p.rapidapi.com";
     const method = "GET";
+    const url = "https://community-open-weather-map.p.rapidapi.com/weather" + params;
 
-    let url = "https://community-open-weather-map.p.rapidapi.com/weather";
-    url += "?";
-    url += "lat" + "=" + latitude + "&";
-    url += "lon" + "=" + longitude;
-
-    console.log("lat:" + latitude + " lon:" + longitude);
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-    }
-
-    xhr.open(method, url);
+    xhr.onload = func;
+    xhr.responseType = "json";
+    xhr.open(method, url, async);
     xhr.setRequestHeader("x-rapidapi-key", api_key);
     xhr.setRequestHeader("x-rapidapi-host", host);
+    return xhr;
+}
+
+function makeCoordsWeatherRequest(latitude, longitude, func) {
+    const params = "?" + "lat" + "=" + latitude + "&" + "lon" + "=" + longitude;
+    return makeWeatherRequest(params, func);
+}
+
+function makeCityWeatherRequest(cityName, func) {
+    const params = "?" + "q" + "=" + cityName;
+    return makeWeatherRequest(params, func);
+}
+
+function makeSourceWeatherRequest(source, func) {
+    if (source.byCity) {
+        return makeCityWeatherRequest(source.cityName, func);
+    } else {
+        return makeCoordsWeatherRequest(source.latitude, source.longitude, func);
+    }
+}
+
+function sendWeatherRequest(xhr, func) {
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            func();
+        }
+    }
     xhr.send();
 }
