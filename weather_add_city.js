ae323d69b2bc84a924fd5aed3ca409c46396e442
sheensekai <sheensekai@ya.ru>
2021-03-08 19:16:59 +0300

    Make w_add_city module use the server

--- weather_add_city.js
+++ weather_add_city.js
@@ -1,34 +1,87 @@
-function addCityWithRequest(source, replacedBlock = null) {
-    const loaderBlock = makeLoaderCityBlock();
-    if (replacedBlock != null) {
-        replaceWeatherBlockFromList(replacedBlock, loaderBlock);
+function replaceLoaderBlockWithNewCityBlock(state, loaderBlock) {
+    const weatherProperties = getRuPropertyListFromState(state);
+    const imgSrc = getIconUrlFromResponseState(state);
+    const weatherBlock = makeWeatherBlock(state.cityName, state.temp, imgSrc, weatherProperties);
+    if (!checkIfWeatherBlockIsAlreadyInList(weatherBlock)) {
+        replaceWeatherBlockFromList(loaderBlock, weatherBlock);
     } else {
-        addWeatherBlockInList(loaderBlock);
+        removeWeatherBlockFromList(loaderBlock);
+        alert("Этот город уже был добавлен в список.")
     }
+}
 
-    const xhr = makeSourceWeatherRequest(source);
-    sendWeatherRequest(xhr, function () {
-            const state = getWeatherStateFromResponse(xhr.response);
-            if (state !== null) {
-                const weatherProperties = getRuPropertyListFromState(state);
-                const imgSrc = getIconUrlFromResponseState(state);
-                const weatherBlock = makeWeatherBlock(state.cityName, state.temp, imgSrc, weatherProperties);
-                if (!checkIfWeatherBlockIsAlreadyInList(weatherBlock)) {
-                    replaceWeatherBlockFromList(loaderBlock, weatherBlock);
-                } else {
-                    removeWeatherBlockFromList(loaderBlock);
-                    alert("Этот город уже был добавлен в список.")
-                }
-            }
-        },
-        function () {
-            removeWeatherBlockFromList(loaderBlock);
-            alert("К сожаление, данные о погоде в указанном городе найти не получилось. Убедитесь, что ввели правильное название.");
-        },
-        function () {
-            removeWeatherBlockFromList(loaderBlock);
-            alert("К сожаление, данные о погоде в указанном городе найти не получилось. Был превышен лимит запросов для приложения. Подождите минуту и добавьте город еще раз.");
-        });
+function addCitySuccess(xhr, loaderBlock) {
+    const state = xhr.response;
+    if (state !== null) {
+        replaceLoaderBlockWithNewCityBlock(state, loaderBlock);
+    }
+}
+
+function addCityFailure(xhr, loaderBlock) {
+    removeWeatherBlockFromList(loaderBlock);
+    alert("К сожаление, данные о погоде в указанном городе найти не получилось. Убедитесь, что ввели правильное название.");
+}
+
+function addCityTooManyRequests(xhr, loaderBlock) {
+    removeWeatherBlockFromList(loaderBlock);
+    alert("К сожаление, данные о погоде в указанном городе найти не получилось. Был превышен лимит запросов для приложения. Подождите минуту и добавьте город еще раз.");
+}
+
+function addCityReceiveResponse(xhr, loaderBlock) {
+    if (xhr.status === 200) {
+        addCitySuccess(xhr, loaderBlock);
+    } else if (xhr.status === 404) {
+        addCityFailure(xhr, loaderBlock);
+    } else if (xhr.status === 429) {
+        addCityTooManyRequests(xhr, loaderBlock);
+    }
+}
+
+function updateCitySuccess(xhr, cityName, loaderBlock, weatherBlock) {
+    const states = xhr.response;
+    let found = false;
+    for (let state of states) {
+        if (states.cityName === cityName) {
+            replaceLoaderBlockWithNewCityBlock(state, loaderBlock);
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        updateCityFailure(xhr, cityName, loaderBlock, weatherBlock);
+    }
+}
+
+function updateCityFailure(xhr, cityName, loaderBlock, weatherBlock) {
+    replaceWeatherBlockFromList(loaderBlock, weatherBlock);
+    alert("К сожалению, обновить данные о данном городе не получилось.");
+}
+
+function updateCityTooManyRequests(xhr, cityName, loaderBlock, weatherBlock) {
+    replaceWeatherBlockFromList(loaderBlock, weatherBlock);
+    alert("К сожалению, обновить данные о данном городе не получилось, поскольку был превышен лимит посылаемых запросов. Попробуйте еще раз через минуту.");
+}
+
+function updateCityReceiveResponse(xhr, cityName, loaderBlock, weatherBlock) {
+    if (xhr.status === 200) {
+        updateCitySuccess(xhr, cityName, loaderBlock, weatherBlock);
+    } else if (xhr.status === 404) {
+        updateCityFailure(xhr, cityName, loaderBlock, weatherBlock);
+    } else if (xhr.status === 429) {
+        updateCityTooManyRequests(xhr, cityName, loaderBlock, weatherBlock);
+    }
+}
+
+function addCityWithRequest(source) {
+    const loaderBlock = makeLoaderCityBlock();
+    addWeatherBlockInList(loaderBlock);
+    addFavouriteRequest(source.cityName, (xhr) => addCityReceiveResponse(xhr, loaderBlock));
+}
+
+function updateCityWithRequest(source, weatherBlock) {
+    const loaderBlock = makeLoaderCityBlock();
+    replaceWeatherBlockFromList(weatherBlock, loaderBlock);
+    getFavouritesRequest((xhr) => updateCityReceiveResponse(xhr, source.cityName, loaderBlock, weatherBlock));
 }
 
 function addCityButtonClick() {
@@ -42,5 +95,5 @@ function addCityButtonClick() {
 function updateCityButtonClick(weatherBlock) {
     const cityName = weatherBlock.getElementsByClassName("wtr-city-name")[0].innerText;
     const source = {byCity: true, cityName: cityName};
-    addCityWithRequest(source, weatherBlock);
+    updateCityWithRequest(source, weatherBlock);
 }
